c01097341475c2b83c53df3e4839ad57f4844e23
hesham <heshamabdulzaher@gmail.com>
2018-11-29 19:28:02 +0200

    Add some colors

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let cards = document.querySelectorAll(".card");
 cards.forEach(singleCard => {
+  handleCard(singleCard);
   // Mouse enter
   singleCard.addEventListener("mouseenter", e => {
     let copyIcon = document.createElement("img");
@@ -8,14 +9,41 @@ cards.forEach(singleCard => {
   });
   // Mouse leave
   singleCard.addEventListener("mouseleave", e => {
-    document.querySelector(".square_color img").remove();
+    let copyIcon = document.querySelector(".square_color img");
+    if (copyIcon) {
+      copyIcon.remove();
+    }
   });
   // Click to copy
-  singleCard.addEventListener("click", e => {
-    e.target.closest(".card");
+  singleCard.querySelector(".square_color").addEventListener("click", e => {
+    document.querySelector(".square_color img").remove();
     let youJustCopied = document.createElement("div");
     youJustCopied.classList.add("you_just_copied");
+    youJustCopied.style.backgroundColor = singleCard.getAttribute("data-color");
     youJustCopied.innerHTML = `<div><img src="./img/checked.svg"><p>You got it!</p></div>`;
     singleCard.appendChild(youJustCopied);
+    let colorCode = singleCard.querySelector(".color_code");
+    colorCode.select();
+    document.execCommand("copy");
+    clearSelection();
+
+    setTimeout(() => {
+      youJustCopied.remove();
+    }, 1500);
   });
 });
+
+function handleCard(card) {
+  console.log(card);
+  let color = card.getAttribute("data-color");
+  card.querySelector(".square_color").style.backgroundColor = color;
+  card.querySelector(".color_code").setAttribute("value", color);
+}
+
+function clearSelection() {
+  if (window.getSelection) {
+    window.getSelection().removeAllRanges();
+  } else if (document.selection) {
+    document.selection.empty();
+  }
+}
