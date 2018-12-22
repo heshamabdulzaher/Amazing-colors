43f6181954024b502dfcecbd12036275215e61f3
hesham <heshamabdulzaher@gmail.com>
2018-12-22 23:48:56 +0200

    Create colors array and create card for every color

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+const container = document.querySelector(".container");
+const colors = [
+  "#6d23b8",
+  "#ffc600",
+  "#09181E",
+  "#37A000",
+  "#0255e9",
+  "#073F54",
+  "#0C2129",
+  "#FE4C00",
+  "#3498db",
+  "#E33832",
+  "#4C5175",
+  "#E1E8EC",
+  "#BCE7D9"
+];
+// Create div for each color, Append all of them in their container
+colors.forEach(color => {
+  let newEl = `<div class="card" data-color="${color}">
+  <div class="square_color"></div>
+  <input type="text" readonly class="color_code"/>
+</div>`;
+  container.innerHTML += newEl;
+});
+
+// Hover Animation
 let cards = document.querySelectorAll(".card");
 cards.forEach(singleCard => {
   handleCard(singleCard);
@@ -33,12 +59,14 @@ cards.forEach(singleCard => {
   });
 });
 
+// Get the data-color attr from each card and Set it as background color.
 function handleCard(card) {
   let color = card.getAttribute("data-color");
   card.querySelector(".square_color").style.backgroundColor = color;
   card.querySelector(".color_code").setAttribute("value", color);
 }
 
+// Clear selection after the user copy the color code
 function clearSelection() {
   if (window.getSelection) {
     window.getSelection().removeAllRanges();
